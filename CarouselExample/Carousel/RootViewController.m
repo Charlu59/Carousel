d3df602ff7695eb7a2d9a13375c89627f3ad71ad
Charlu59 <charles-hubertbasuiau@MacBook-Pro-de-Charles-Hubert.local>
2013-10-25 11:58:38 +0200

    suppression commentaire

--- CarouselExample/Carousel/RootViewController.m
+++ CarouselExample/Carousel/RootViewController.m
@@ -20,7 +20,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -28,7 +27,6 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)viewDidLoad
